5659a1fb9db9f51595687a4b25916fffa56f27fa
LaaKii <kai-achertal@gmx.de>
2021-05-25 09:16:31 -0700

    implemented rules for value object

--- src/main/java/backend/searchAlgorithms/Field.java
+++ src/main/java/backend/searchAlgorithms/Field.java
@@ -2,6 +2,8 @@ package backend.searchAlgorithms;
 
 import frontend.Button;
 
+import java.util.Arrays;
+
 public final class Field {
     private final Button[][] twoDimensionalField;
     private final Button[][][] threeDimensionalField;
@@ -17,11 +19,31 @@ public final class Field {
     }
 
     public Button[][] getTwoDimensionalField() {
+        if (twoDimensionalField==null){
+            throw new RuntimeException("Wrong constructor invoked. Returning null object now");
+        }
         return twoDimensionalField;
     }
 
     public Button[][][] getThreeDimensionalField() {
+        if (threeDimensionalField==null){
+            throw new RuntimeException("Wrong constructor invoked. Returning null object now");
+        }
         return threeDimensionalField;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Field field = (Field) o;
+        return Arrays.equals(twoDimensionalField, field.twoDimensionalField) && Arrays.equals(threeDimensionalField, field.threeDimensionalField);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Arrays.hashCode(twoDimensionalField);
+        result = 31 * result + Arrays.hashCode(threeDimensionalField);
+        return result;
+    }
 }
